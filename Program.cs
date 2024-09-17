db798a364f945c6fba80dbb2fc2e0c081c4b6edc
Ivan Sevilla <maueth515@gmail.com>
2024-09-16 21:18:53 -0500

    la2

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ do
     Console.WriteLine("G) Mostrar los nombres de personas almacenados en una lista predefinida");
     Console.WriteLine("S) Salir");
 
-    opcion = Console.ReadLine();
+    opcion = Console.ReadLine().ToUpper();
 
     int numero = 0;
 
@@ -26,6 +26,33 @@ do
             Console.WriteLine("Es valor del número insertado");
             ConsultarNumero(numero);
             break;
+
+        case "B":
+            Console.WriteLine("ingrese un valor del 1 al 7");
+            numero = Convert.ToInt32(Console.ReadLine());
+            MostrarDiaSemana(numero);
+            break;
+
+        case "C":
+            Console.WriteLine("");
+            break;
+
+        case "D":
+            Console.WriteLine("");
+            break;
+
+        case "E":
+            Console.WriteLine("");
+            break;
+
+        case "F":
+            Console.WriteLine("");
+            break;
+
+        case "G":
+            Console.WriteLine("");
+            break;
+
 
         default:
             break;
@@ -44,7 +71,41 @@ do
 
 
 
-//a
+
+//B
+void MostrarDiaSemana(int valor)
+{
+    //string valor2 = Convert.ToString(numero);
+    switch (valor)
+    {
+        case 1:
+            Console.WriteLine("Día elegido es: LUNES ");
+            break;
+        case 2:
+            Console.WriteLine("Día elegido es: MARTES ");
+            break;
+        case 3:
+            Console.WriteLine("Día elegido es: MIERCOLES");
+            break;
+        case 4:
+            Console.WriteLine("Día elegido es: JUEVES ");
+            break;
+        case 5:
+            Console.WriteLine("Día elegido es: VIERNES ");
+            break;
+        case 6:
+            Console.WriteLine("Día elegido es: SÁBADO ");
+            break;
+        case 7:
+            Console.WriteLine("Día elegido es: DOMINGO ");
+            break;
+        default:
+            break;
+    }
+}
+
+
+//A
 int ConsultarNumero(int numero)
 {
     if (numero == 0)
